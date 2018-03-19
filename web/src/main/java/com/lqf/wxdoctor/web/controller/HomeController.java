ab3f5cf7830f981f2683128dba9ac4c76a4f4b87
chaos <604648286@qq.com>
2018-03-19 21:13:58 +0800

    [change] 框架分层修改

--- web/src/main/java/com/lqf/wxdoctor/web/controller/HomeController.java
+++ web/src/main/java/com/lqf/wxdoctor/web/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.lqf.wxdoctor.web.controller;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.lqf.wxdoctor.dao.AccountDao;
 import com.lqf.wxdoctor.model.Account;
+import org.apache.commons.logging.Log;
+import org.apache.ibatis.logging.log4j2.Log4j2AbstractLoggerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +22,9 @@ public class HomeController {
 
     @GetMapping("/")
     public List<Account> index() {
+        PageHelper.startPage(1, 2).doSelectPage(() -> accountDao.find(1, "dd"));
         List<Account> accounts = accountDao.find(1, "d");
+        long total = ((Page) accounts).getTotal();
         return accounts;
     }
 
